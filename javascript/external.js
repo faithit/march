f0d625f30ed4fcc05993a6562a7387b7ca317f30
faithit <faithittrainer@gmail.com>
2024-04-09 11:40:49 +0300

    js

---
+++ javascript/external.js
@@ -0,0 +1,13 @@
+document.write("hello from external js file <br>")
+console.log("hello from console in external js file");
+/*declaring a variable
+//var variablename
+//asigning a value
+variablename=value*/
+// var fname;
+// fname="jane";
+var fname="jane";
+var age=20;
+//displaying a variable
+document.write("hello ",fname," you are ",age ," years old <br>");
+console.log("good morning ",fname);
